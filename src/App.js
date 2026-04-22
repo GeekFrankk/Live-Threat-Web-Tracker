c1da13431c39ac9f86a5ae231b108421790b0924
ClassBetances <158072877+ClassBetances@users.noreply.github.com>
2026-04-22 02:50:19 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [threats, setThreats] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/threats")
+      .then(res => res.json())
+      .then(data => setThreats(data))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Threat Data</h1>
+
+      {threats.map(threat => (
+        <div key={threat.id} style={{ border: "1px solid black", margin: "10px", padding: "10px" }}>
+          <p><b>IP:</b> {threat.ip}</p>
+          <p><b>Location:</b> {threat.city}, {threat.state}</p>
+          <p><b>Severity:</b> {threat.severity}</p>
+          <p><b>Type:</b> {threat.type}</p>
+        </div>
+      ))}
     </div>
   );
 }
